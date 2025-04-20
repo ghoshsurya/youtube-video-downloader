12c52ea67950bd0f44091db2a43dc651238927d3
SURYAKANTA GHOSH <100485220+ghoshsurya@users.noreply.github.com>
2025-04-20 20:53:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,5 +34,9 @@ def download():
     except Exception as e:
         return f"<h3>Error downloading video: {str(e)}</h3>"
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
